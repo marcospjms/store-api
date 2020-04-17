d80e5b54db3a95e5d654c6db3c670b0c4d69de04
marcospjms <marcos182p@gmail.com>
2020-04-16 23:57:49 -0300

    Continuando modelagem da classe de desconto

--- src/main/java/br/com/store/model/Discount.java
+++ src/main/java/br/com/store/model/Discount.java
@@ -1,8 +1,13 @@
 package br.com.store.model;
 
+import br.com.store.util.DateTimeDeserializer;
+import br.com.store.util.DateTimeSerializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.joda.time.DateTime;
 
 import javax.persistence.*;
 import java.util.List;
@@ -15,6 +20,12 @@ import java.util.stream.Collectors;
 @NoArgsConstructor
 public class Discount extends AbstractEntity {
 
+    private String code;
+    /**
+     * Taxa de desconto. Se discountType for RELATIVE, deverá ser entre 0 e 1. Se for ABSOLUTE poderá ser qualquer valor.
+     */
+    private double discountRate;
+
     @Enumerated(EnumType.STRING)
     private PaymentType paymentTypes;
 
@@ -26,15 +37,49 @@ public class Discount extends AbstractEntity {
 
     private boolean cumulative;
 
-    private double discountRate;
+    private boolean toShoppingCart;
 
+    @JsonSerialize(using = DateTimeSerializer.class)
+    @JsonDeserialize(using = DateTimeDeserializer.class)
+    private DateTime start;
+
+    @JsonSerialize(using = DateTimeSerializer.class)
+    @JsonDeserialize(using = DateTimeDeserializer.class)
+    private DateTime end;
+
+    private long counter;
+
+    private long maxCounter;
+
+    public void setDiscountRate(double discountRate) {
+        if (this.discountType == null) {
+            throw new RuntimeException("Impossível definir a taxa de desconto: sem nenhum discountType definido");
+        }
+        this.checkDiscountRate(discountRate);
+        this.discountRate = discountRate;
+    }
 
     public double calculate(List<Product> products, PaymentType paymentType) {
         return this.filterProducts(products).stream().reduce(
                 0.0,
-                (subtotal, product) -> subtotal + this.getDiscount(product, paymentType),
+                (subtotal, product) -> subtotal + this.calcDiscountByProduct(product, paymentType),
                 Double::sum
         );
+    }
+
+    private void checkDiscountRate(double discountRate) {
+        switch (this.discountType) {
+            case ABSOLUTE:
+                break;
+            case RELATIVE:
+                if (discountRate < 0 || discountRate > 1) {
+                    throw new RuntimeException("Impossível definir a taxa de desconto: discountType RELATIVE só " +
+                            "permite valor entre 0 e 1");
+                }
+                break;
+            default:
+                throw new RuntimeException("Impossível definir a taxa de desconto: discountType desconhecido");
+        }
     }
 
     private List<Product> filterProducts(List<Product> products) {
@@ -44,7 +89,7 @@ public class Discount extends AbstractEntity {
         return products.stream().filter((product -> this.category.equals(product.getCategory()))).collect(Collectors.toList());
     }
 
-    private double getDiscount(Product product, PaymentType paymentType) {
+    private double calcDiscountByProduct(Product product, PaymentType paymentType) {
         return product.getPrice();
     }
 }
