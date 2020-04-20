f160d03dfd5a5f243e44617a8f89d7bc3c162158
marcospjms <marcos182p@gmail.com>
2020-04-19 22:34:56 -0300

    Evoluindo teste com mockito

--- src/test/java/br/com/store/services/ShoppingCartServiceTest.java
+++ src/test/java/br/com/store/services/ShoppingCartServiceTest.java
@@ -2,28 +2,32 @@ package br.com.store.services;
 
 import static org.junit.Assert.*;
 
-import br.com.store.model.Category;
+import br.com.store.model.Product;
+import br.com.store.model.ShoppingCart;
+import br.com.store.model.ShoppingCartProduct;
+import br.com.store.model.auth.StoreUser;
 import br.com.store.repositories.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.*;
+
 import static org.mockito.Mockito.when;
+
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.Optional;
+import java.util.Arrays;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ShoppingCartServiceTest {
+
+    @Mock
+    private StoreUserRepository storeUserRepository;
 
     @Mock
     private CategoryRepository categoryRepository;
 
     @Mock
     private DiscountRepository discountRepository;
-
-    @Mock
-    private ProductRepository productRepository;
 
     @Mock
     private ShoppingCartDiscountRepository shoppingCartDiscountRepository;
@@ -34,17 +38,40 @@ public class ShoppingCartServiceTest {
     @Mock
     private ShoppingCartRepository shoppingCartRepository;
 
-    @InjectMocks
-    private CategoryService categoryService;
+    @Mock
+    private StoreUserService storeUserService;
+
+    @Mock
+    private ProductService productService;
 
     @InjectMocks
     private ShoppingCartService shoppingCartService;
 
     @Test
-    public void sampleTeste() {
-        Category category = new Category();
-        category.setId(1L);
-        when(this.categoryRepository.findById(1L)).thenReturn(Optional.of(category));
+    public void addProductTest() {
+        Long productId = 0l;
+        Long userId = 1l;
+        Long shoppingCartId = 2l;
+        String username = "marcospjms";
+        Product product = new Product();
+        product.setId(productId);
+        StoreUser storeUser = new StoreUser();
+        storeUser.setId(1l);
+        storeUser.setUsername(username);
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.setId(shoppingCartId);
+        when(this.productService.findById(1L)).thenReturn(product);
+        when(this.storeUserService.findByUsername(username)).thenReturn(storeUser);
+        when(this.shoppingCartRepository.findByStoreUserId(userId)).thenReturn(Arrays.asList(shoppingCart));
+
+        ShoppingCartProduct shoppingCartProduct = this.shoppingCartService.addProduct("marcospjms", 1L);
+
+        assertEquals(1L, 1L);
+    }
+
+    @Test
+    public void addDiscountTest() {
+        ShoppingCartProduct shoppingCartProduct = this.shoppingCartService.addProduct("marcospjms", 1L);
 
         assertEquals(1L, 1L);
     }
